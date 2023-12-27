bcaf5ee235fe06e07a331340f9a70b4ea0b1d257
RobertoBaiochi <robertombbaiocho@gmail.com>
2023-12-27 15:30:42 -0300

    feat: nextauth config

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+import Head from "next/head";
+
+export default function Dashboard() {
+    return (
+        <>
+            <Head>
+                <title>Painel de Tarefas</title>
+            </Head>
+
+            <main>
+                <h1>Página Painel</h1>
+            </main>
+        </>
+    );
+}
